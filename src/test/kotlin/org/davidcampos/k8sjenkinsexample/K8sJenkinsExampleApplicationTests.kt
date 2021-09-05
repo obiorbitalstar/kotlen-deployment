6fc7b3141784098fe4ae037753cef4c124eaf299
obiorbitalstar <saed@saed-alkhateeb-pc.scoiumtech.local>
2021-09-05 16:40:39 +0300

    arg3 7ali mos

--- src/test/kotlin/org/davidcampos/k8sjenkinsexample/K8sJenkinsExampleApplicationTests.kt
+++ src/test/kotlin/org/davidcampos/k8sjenkinsexample/K8sJenkinsExampleApplicationTests.kt
@@ -1,10 +1,10 @@
-package com.example.demo
+package org.davidcampos.k8sjenkinsexample
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DemoApplicationTests {
+class K8sJenkinsExampleApplicationTests {
 
 	@Test
 	fun contextLoads() {
